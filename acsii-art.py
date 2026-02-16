e78e2d16051e302b9550418c0131324b12bccfb0
Your Name <you@example.com>
2026-02-17 00:42:51 +0530

    Add acsii art kt

---
+++ acsii-art.py
@@ -0,0 +1,8 @@
+print("  ********  ")
+print(" *         * ")
+print("*  ^    ^    *")
+print("*     >       *")
+print("*  \\____/   *" )
+print(" *           * ")
+print("  ***********  ")
+print("Hi Welcome to a Bot I just smile")
