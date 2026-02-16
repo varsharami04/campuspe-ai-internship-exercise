f1821944553113f9c71b5ba9fe4ca4ed8c1f9f20
Your Name <you@example.com>
2026-02-17 00:49:58 +0530

    Added Variables

---
+++ variables.py
@@ -0,0 +1,33 @@
+#Allowed
+myage = 21
+my_age = 21
+_my_age = 21
+myAge = 21
+MYAGE = 21
+myage1 = 21
+
+#Wrong/Illegal
+#1myage = 21
+#my-var = 21
+#my var = 21
+
+#Assignments
+a,b,c = "varsha","siri","nag"
+print(a)
+print(b)
+print(c)
+
+d=e=f = "varsha"
+print(d)
+print(e)
+print(f)
+
+
+name="varsha"
+age = 21
+height = 5.2
+is_married =True
+
+print("Hi I am ",name)
+print("I am ",age,"Years old")
+print("My height is ",height,"feet")
