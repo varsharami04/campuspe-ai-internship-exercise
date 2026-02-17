d034bd4c95194ff71aca6871ecebeedf80f76c72
Your Name <you@example.com>
2026-02-17 12:10:05 +0530

    Added Operators and user-input.

---
+++ operators.py
@@ -0,0 +1,36 @@
+#Arithmatic Operators
+#+ print(a+b)
+#- print(a-b)
+#* print(a*b)
+#/ print(a/b)
+#% print(a%b)
+#**print(a**b)
+
+#comparsion operators
+
+# == -> Equal to       ->print(x==y)
+# != -> not equal to   ->print(x!=y)
+# > -> greater than    ->print(x>y)
+# < -> lesser than      ->print(x<y)
+# >= -> greater than or equal to  ->print(x>=y)
+# <= -> lesser than or equal to    ->print(x<=y)
+
+#logical operators
+#and  -> AND operator
+#or   -> OR operator
+#not  -> NOT operator
+
+#Assignment operators
+#a=120
+#a=a+1
+#a+=1
+#a-=1
+#a*=1
+#a/=1
+
+#PEMDAS
+#(Parenthesis , Exponents, Multiply/Divide, Add/Subtract)
+
+result = (2 + 3) * 4
+print(result)
+
---
+++ user-input .py
@@ -0,0 +1,3 @@
+age_str=input("What's your age?")
+age=int(age_str)
+print(age+1)
