40875919504d178536ebae2e114aba32fb25fdea
Your Name <you@example.com>
2026-02-20 01:32:02 +0530

    Added conditional  statements

---
+++ conditional-statements.py
@@ -0,0 +1,76 @@
+#name_str = input("Hi what is your name?")
+#age=input("Hi" +name_str+ "what is your age?")
+
+#password = "123123321"
+# if else condition 
+#if password=="secret@123":
+    #print("Access Granted")
+#else:
+    #print("Access Denied")
+#if int(age) >=18:
+    #print("You are a Adult")
+    #print("You can Vote!")
+#else :
+    #print("You are a bacha")   
+    #print("Go have ice cream")
+
+# IF else if 
+# score =85
+# if score >=90:
+#     grade='A'
+# elif score >=80:
+#     grade ='B'
+# elif score >=70:
+#     grade = 'C'
+# elif score >=60:
+#     grade = 'D'
+# else :
+#     grade = 'F'
+# print("Your Grade is :",grade)
+
+#Nested IF Else
+# age =25
+# has_license=False
+
+# if age>=18:
+#     print("You are old enough to drive")
+#     if has_license:
+#         print("You can drive!")
+#     else:
+#         print("But you need a license")
+# else:
+#     print("Too young to drive")
+
+# If else using "AND"
+# age=20
+# has_ticket=False
+# if age>=18 or has_ticket:
+#     print("You can enter the concert")
+# else:
+#     print("Cannot Enter")
+
+# If else using "OR"
+# day="monday"
+
+# if day=="saturday" or day=="sunday":
+#     print("It 's the weekend! Open the book")
+# else:
+#     print("It's weekday")
+
+# If else using "NOT"
+# is_sunny=True
+
+# if not is_sunny:
+#     print("It's sunny")
+# else:
+#     print("It's cloudy")
+#Ternary operator
+age =21
+
+# if age>=18:
+#     status="adult"
+# else:
+#     status="minor"
+
+status = "adult" if age >=18 else "minor"
+print(status)
